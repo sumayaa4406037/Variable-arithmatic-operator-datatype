348b9c1df2818e283a4db9f8a0fb4eab3defb868
sumayaa4406037 <149380178+sumayaa4406037@users.noreply.github.com>
2024-01-19 19:12:25 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,37 @@
+// var x=50;
+// // document.querySelector('h1').innerHTML=x;
+// // window.alert(x);
+// // document.write(x)
+// // console.log(x)
+// var x=50;
+// var y=2;
+// var total=x+y;
+// var diffrent=x-y;
+// var div=x/y;
+// var mul=x*y;
+// var multiple=x**y;
+// document.write(multiple);
+// var x=10;
+// var y=3;
+// var reaminder=x%y;
+// document.write(reaminder);
+
+
+
+// function myFunction(x,y){
+//  var result=x+y+'<br>'+x*y;
+//  return result;
+// }
+// document.write(myFunction(10,20));
+
+// function myFunction(x,y){
+// var result=x+y+'<br>'+x*y;
+// document.write(result);
+//  }
+//  myFunction(10,40);
+
+// var mainNumber=Number (prompt('Type Your Number'));
+// document.write(Math.pow (mainNumber,2));
+
+// var ran=Math.round(Math.random()*100+5);
+// document.write(ran);
